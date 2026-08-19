180a406fe55546b3e5d5ea772bd0daf98ba4a004
David Laganiere <40720561+davidlag0@users.noreply.github.com>
2026-08-19 17:01:12 -0400

    feat: add blog

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,8 @@
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 export default function(eleventyConfig) {
-  eleventyConfig.addBundle("css");
   eleventyConfig.addPassthroughCopy({ "public": "/" });
+  eleventyConfig.addPassthroughCopy({ "src/css": "/css" });
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
     formats: ["webp"],
     widths: [224, 448],
@@ -18,13 +18,23 @@ export default function(eleventyConfig) {
     },
   });
 
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/_posts/**/*.md")
+      .filter(post => !post.data.draft)
+      .sort((a, b) => b.date - a.date);
+  });
+
+  eleventyConfig.addFilter("dateFormat", function(date) {
+    return date.toISOString().split("T")[0];
+  });
+
   return {
     dir: {
       input: "src",
       output: "_site",
       includes: "_includes"
     },
-    templateFormats: ["liquid", "html"],
+    templateFormats: ["liquid", "html", "md"],
     htmlTemplateEngine: "njk",
     passthroughFileCopy: true
   };
